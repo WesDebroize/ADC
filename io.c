a558fe4d8a591b093ee93cee89463c17822a536c
wesde <wesley2.debroize@live.uwe.ac.uk>
2026-07-10 15:20:49 +0100

    Changed the names of different variables to match the names within sensor log file - done to clean up the code and make it more readable before adding anymore code (few variables were named to match the pp). Removed checking the size of the header from io.c as it wasnt reachable and didn't seem needed. Within main header_checker and load_records functions are called and checked they worked.

--- io.c
+++ io.c
@@ -7,39 +7,32 @@
 #include "ADC.h"
 
 //Read and Validate the header
-int header_checker(const char filename[], io_header *header) {
+int header_checker(const char filename[], File_header *header) {
     FILE *data_file = fopen(filename, "rb");    //rb - read in binary
     if (data_file == NULL) {
         printf("File couldnt open %s\n", filename); //Returns NULL if file couldnt be opened
         return 0;
     }
-    //Checking if io_header is the correct size
-    if (sizeof(io_header) != 24) {
-        printf("Header isnt 24 bytes, its instead %zu bytes\n", sizeof(io_header));
-        fclose(data_file);
-        return 0;
-    }
-
     /*
      Reads one 24-byte header (size of io_header) from the data_file and stores it into the
      io_header structure pointed to by header
 
      Didnt use &header as that would be the address of the pointer, not the header struct
      */
-    if (fread(header, sizeof(io_header), 1, data_file) != 1) {  //checks that one complete io_header was read from the file
-        printf("Couldnt read io_header\n");
+    if (fread(header, sizeof(File_header), 1, data_file) != 1) {  //checks that one complete io_header was read from the file
+        printf("Couldnt read File_header\n");
         fclose(data_file);
         return 0;
     }
     //Checking magic number is 0xADC1BEEF
-    if (header->magic != MAGIC_NUMBER) {
-        printf("Expected 0x%08X, instead got 0x%08X\n",MAGIC_NUMBER,header->magic); //%08X converts the integer to hexidecimal to 8 digits
+    if (header->Magic_number!= MAGIC_NUMBER) {
+        printf("Expected 0x%08X, instead got 0x%08X\n",MAGIC_NUMBER,header->Magic_number); //%08X converts the integer to hexidecimal to 8 digits
         fclose(data_file);
         return 0;
     }
     //The file is opened, it's the correct size, it could read the header, magic number is correct
     fclose(data_file);
-    printf("io_header read successfully, magic number is 0x%08X\n",header->magic);
+    printf("File_header read successfully, magic number is 0x%08X\n",header->Magic_number);
     return 1;
 }
 
--- main.c
+++ main.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "ADC.h"
 #include "io.h"
 
-int main(void)
-{
-    printf("Size of ADCSample: %zu bytes\n", sizeof(io_header));
+int main(void){
+    FILE *data_file = fopen("adc_sensor_log.bin", "rb");
+    /*test to see why file cant be opened
+    if (data_file == NULL) {
+        perror("couldnt open file");
+        return 0;
+    }
+    */
+   // printf("Size of ADCSample: %zu bytes\n", sizeof(io_header)); - used to check size of both headers
+    File_header header; //Variable to store the 24-byte file header
+    //Calls header_checker function from io.c
+   if (header_checker("adc_sensor_log.bin", &header) == 0) {
+       printf("Header validation failed\n");
+       fclose(data_file);
+       return 0;
+   }
+    //printing header to check all info and size is correct
+    printf("Magic no.: %x\n", header.Magic_number);
+    printf("Version %u\n", header.File_version);
+    printf("Channels: %u\n", header.Channel_count);
+    printf("Record count: %u\n", header.Record_count);
+    printf("Sample rate: %uhz\n", header.Sample_rate);
+    printf("Size of header: %zu bytes\n", sizeof(header));
 
-    return 0;
+
+
+    ADCSample *samples = load_records(data_file, header.Record_count);
+    if (samples == NULL) {
+        printf("Failed to load ADC records\n");
+        fclose(data_file);
+        return 0;
+    }
+    printf("ADC records loaded successfully\n");
+    fclose(data_file);
+    free(samples);
+    return 1;
 }
